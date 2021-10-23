d5e1c5fd7c2c83fb31d9ed8384c86f5e765cf991
Peter Vecera <vecerapeto@gmail.com>
2021-10-23 16:16:49 +0200

    Vue lekcia 1 Itnetwork

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import CalculatorForm from './calculator/CalculatorForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [titleState] = useState('React kalkulačka');
+    const [xState, setXState] = useState(0);
+    const [yState, setyState] = useState(0);
+
+    const inputXonchange = (event) => {
+        console.log(event.target.value);
+        setXState(event.target.value);
+    };
+
+    const inputYonchange = (event) => {
+        console.log(event.target.value);
+        setyState(event.target.value);
+    };
+
+    return (
+        <div className="App">
+            <h1>{titleState}</h1>
+            <CalculatorForm
+                xOnChange={inputXonchange}
+                yOnChange={inputYonchange}
+                x={xState}
+                y={yState}
+            />
+        </div>
+    );
+};
 
 export default App;
